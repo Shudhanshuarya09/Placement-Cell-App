e8955c97579879c9d8755808f4bb50845d86f0fe
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-10-07 23:35:07 +0530

    Admin Panel ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import { useState } from 'react';
-import Header from './components/header/Header';
+// import { useState } from 'react';
+// import Header from "./components/header/Header";
+// import Sidebar from './components/sidebar/Sidebar';
+// import Home from './components/home/Home'
+// import './App.css'
 
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <Header />
+//       <Sidebar/>
+//       <Home />
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
+import Home from './components/home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
 
   return (
-    <>
-      <Header />
-    </>
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import './Header.css';
+import 
+ {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
+ from 'react-icons/bs'
 
-const Header = () => {
+function Header({OpenSidebar}) {
   return (
-    <header>
-      <nav className="navbar">
-        <div className="logo">
-          <h1>Placement Cell</h1>
+    <header className='header'>
+        <div className='menu-icon'>
+            <BsJustify className='icon' onClick={OpenSidebar}/>
         </div>
-        <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#alumni">Alumni</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-        <div className="nav-button">
-          <button>Login</button>
+        <div className='header-left'>
+            <BsSearch  className='icon'/>
         </div>
-      </nav>
+        <div className='header-right'>
+            <BsFillBellFill className='icon'/>
+            <BsFillEnvelopeFill className='icon'/>
+            <BsPersonCircle className='icon'/>
+        </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
